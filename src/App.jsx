ff95ecde13b42cf346d1e6cbee5286ec4f734460
zhaoding <18502962139@163.com>
2025-06-06 09:49:14 +0800

    Refactor App component to use Ant Design and add message functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-/**
- * * @file App.jsx
- * 应用入口文件
- */
+import React from 'react';
 
-import React from 'react'
+import { Button, ConfigProvider, message } from "antd";
 
 export default function App() {
-    return (
+    const [ messageApi, contextHolder ] = message.useMessage();
+    const handleClick = () => {
+        messageApi.info("这是一个信息提示！" + new Date().toLocaleTimeString(), 3);
+    }
+  return (
+    <ConfigProvider
+        theme={{
+            token: {
+                colorPrimary: '#00b96b', // 设置主色调
+                borderRadius: 2, // 设置组件圆角
+                colorBgContainer: '#f6ffed', // 设置容器背景色
+            }
+        }}
+    >
         <div>
-            App...
+            {contextHolder}
+            <Button type="primary" onClick={handleClick}>按钮</Button>
+            <Button>默认</Button>
         </div>
-    )
+    </ConfigProvider>
+  )
 }
--- src/index.js
+++ src/index.js
@@ -10,4 +10,6 @@ import App from "./App";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+    <App />
+);
