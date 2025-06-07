79647e86a2162595dc1e06f8fa349fbb8cae85df
zhaoding <18502962139@163.com>
2025-06-07 11:17:55 +0800

    登陆表单

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,29 @@
 import React from 'react';
 
-import { Button, ConfigProvider, message } from "antd";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import { ConfigProvider } from 'antd';
+
+
+import Admin from './pages/admin';
+import Login from './pages/login';
+
 
 export default function App() {
-    const [ messageApi, contextHolder ] = message.useMessage();
-    const handleClick = () => {
-        messageApi.info("这是一个信息提示！" + new Date().toLocaleTimeString(), 3);
-    }
-  return (
-    <ConfigProvider
-        theme={{
-            token: {
-                colorPrimary: '#00b96b', // 设置主色调
-                borderRadius: 2, // 设置组件圆角
-                colorBgContainer: '#f6ffed', // 设置容器背景色
-            }
-        }}
-    >
-        <div>
-            {contextHolder}
-            <Button type="primary" onClick={handleClick}>按钮</Button>
-            <Button>默认</Button>
-        </div>
-    </ConfigProvider>
-  )
+    return (
+        <ConfigProvider
+            theme={{
+                token: {
+                    colorPrimary: '#00b96b', // 设置全局主色调
+                }
+            }}
+        >
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Admin />} />
+                    <Route path="/login" element={<Login />} />
+                </Routes>
+            </BrowserRouter>
+        </ConfigProvider >
+    )
 }
---
+++ src/pages/login/index.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Form, Input } from 'antd';
+
+
+import './index.less';
+import logo from './images/logo.png'
+
+/**
+ * 
+ * @returns 登录组件
+ */
+export default function Login() {
+
+    const onFinish = values => {
+        console.log('Received values of form: ', values);
+    };
+
+    // 自定义密码验证规则
+    const pwdValidator = (_, value) => {
+        if (!value) {
+            return Promise.reject('请输入密码!');
+        } else if (value.length < 4) {
+            return Promise.reject('密码至少4位');
+        } else if (value.length > 12) {
+            return Promise.reject('密码最多12位');
+        } else if (!/^[a-zA-Z0-9_]+$/.test(value)) {
+            return Promise.reject('密码必须是英文、数字或下划线');
+        } else {
+            return Promise.resolve();
+        }
+    }
+
+
+    return (
+        <div className='login'>
+            <header className='login-header'>
+                <img src={logo} alt='logo' />
+                <h1>React 后台管理系统</h1>
+            </header>
+            <section className='login-section'>
+                <h2>用户登陆</h2>
+                <Form
+                    name="login"
+                    initialValues={{ remember: true }}
+                    style={{ maxWidth: 360 }}
+                    onFinish={onFinish}
+                >
+                    <Form.Item
+                        name="username"
+                        rules={[
+                            { required: true, message: '请输入用户名!' },
+                            { min: 4, message: '用户名至少4位' },
+                            { max: 12, message: '用户名最多12位' },
+                            { pattern: /^[a-zA-Z0-9_]+$/, message: '用户名必须是英文、数字或下划线' }
+                        ]}
+                    >
+                        <Input prefix={<UserOutlined style={{ color: 'rgba(0, 0, 0, .25)' }} />} placeholder="用户名" />
+                    </Form.Item>
+                    <Form.Item
+                        name="password"
+                        rules={[
+                            { validator: pwdValidator }
+                        ]}
+                    >
+                        <Input prefix={<LockOutlined style={{ color: 'rgba(0, 0, 0, .25)' }} />} type="password" placeholder="密码" />
+                    </Form.Item>
+                    <Form.Item>
+                        <Button block type="primary" htmlType="submit">
+                            登陆
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </section>
+        </div>
+    )
+}
